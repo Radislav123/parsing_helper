c6a0b9ed5b90d210797556ccbada4d2554263501
Vlasov R. V <radislavvlasov@gmail.com>
2023-11-07 16:01:59 +0300

    fix: исправлено хранилище секретов

--- parsing_helper/secret_keeper.py
+++ parsing_helper/secret_keeper.py
@@ -8,7 +8,10 @@ if TYPE_CHECKING:
 
 class SecretKeeper:
     class Module:
-        pass
+        name: str
+        secrets_path: str
+        json: dict
+        secret_keeper: "SecretKeeper"
 
     def __init__(self, settings: "Settings") -> None:
         pass
@@ -19,6 +22,11 @@ class SecretKeeper:
             data = json.load(file)
         return data
 
-    def add_module(self, name: str, settings_path: str):
-        module = type(name, (self.Module,), self.read_json(settings_path))
+    def add_module(self, name: str, secrets_path: str) -> None:
+        json_dict = self.read_json(secrets_path)
+        module = type(name, (self.Module,), json_dict)()
+        module.name = name
+        module.secrets_path = secrets_path
+        module.json = json_dict
+        module.secret_keeper = self
         setattr(self, name, module)
